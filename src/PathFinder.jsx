02d3a90271fd775adf9594283ed3b571c52050bf
Viktor H <viktor.hajdu@brainit.sk>
2023-03-11 22:10:51 +0100

    A* nearly done, buggy long distance

--- src/PathFinder.jsx
+++ src/PathFinder.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import aStar from './alorithms/aStar';
 import dijkstra from './alorithms/djikstra';
 import Bar from './components/Bar';
 import Node from './components/Node'
@@ -10,7 +11,7 @@ import Node from './components/Node'
 
   for (let i = 0; i < x; i++) {
     for (let j = 0; j < y; j++) {
-      grid.push({num: (i * y + j), status: 3, distance: Infinity, prev: null})
+      grid.push({num: (i * y + j), status: 3, F: Infinity, prev: null, G: Infinity, H: Infinity,})
     }
   }
 
@@ -20,99 +21,98 @@ import Node from './components/Node'
 
 
 
-const PathFinder = () => {
-  const [mode, setMode] = useState(0);
-  const [alg, setAlg] = useState(0);
-  const [matrix, setMatrix] = useState(grid);
-  const [disabledChoice, setDisabledChoice] = useState(false);
+  const PathFinder = () => {
+    const [mode, setMode] = useState(0);
+    const [alg, setAlg] = useState(1);
+    const [matrix, setMatrix] = useState(grid);
+    const [disabledChoice, setDisabledChoice] = useState(false);
 
-  const clearMatrix = () => {
-    setDisabledChoice(false);
-    setMatrix((prev) => {
-      const newMatrix = [...prev]
-      newMatrix.forEach((node) => {
-        node.distance = Infinity;
-        node.prev = null;
-        if (node.status !== 0 && node.status !== 1) {
-          node.status = 3;
-        }
+    const clearMatrix = () => {
+      setDisabledChoice(false);
+      setMatrix((prev) => {
+        const newMatrix = [...prev]
+        newMatrix.forEach((node) => {
+          node.F = Infinity;
+          node.G = Infinity;
+          node.H = Infinity;
+          node.prev = null;
+          if (node.status !== 0 && node.status !== 1) {
+            node.status = 3;
+          }
+        })
+        return newMatrix
       })
-      return newMatrix
-    })
-  }
+    }
 
-
-    const Animate = (result) => {
-      setDisabledChoice(true);
-      const {visitedNodes, path} = result;
-      const processSpeed = 5;
-        for (let i = 0; i < visitedNodes.length; i++) {
+      const Animate = (result, speed, type) => {
+        setDisabledChoice(true);
+        const {visitedNodes, path} = result;
+          for (let i = 0; i < visitedNodes.length; i++) {
+            setTimeout(() => {
+              setMatrix((prev) => {
+                  const newMatrix = [...prev]
+                  if (i === 0) {
+                    newMatrix[visitedNodes[i].num].status = 0
+                    return newMatrix
+                  }
+                  newMatrix[visitedNodes[i].num].status = 5
+                  return newMatrix
+                })
+              }, type === 'djikstra'? speed * i : speed)
+        }
+        
+      setTimeout(() => {
+        if (!path) {
+          alert('No path found!')
+          return;
+        }
+        for (let i = 0; i < path.length; i++) {
           setTimeout(() => {
             setMatrix((prev) => {
-                const newMatrix = [...prev]
-                if (i === 0) {
-                  newMatrix[visitedNodes[i].num].status = 0
-                  return newMatrix
-                }
-                newMatrix[visitedNodes[i].num].status = 5
+              const newMatrix = [...prev]
+              if (i === path.length - 1) {
+                newMatrix[path[i].num].status = 1
                 return newMatrix
-              })
-            }, processSpeed * i)
+              }
+              if (i === 0) {
+                newMatrix[path[i].num].status = 0
+                return newMatrix
+              }
+              newMatrix[path[i].num].status = 6
+              return newMatrix
+            })
+          }, 25 * i)
+        }
+      }, speed * path.length)
+    }
+
+
+
+    const startVisualization = () => {
+      if (alg === 0) {
+        const result = dijkstra(matrix);
+        Animate(result, 5, 'djikstra');
+      }
+      if (alg === 1) {
+        const result = aStar([...matrix]);
+        Animate(result, 15, 'A*');
       }
       
-    setTimeout(() => {
-      if (!path) {
-        alert('No path found!')
-        return;
-      }
-      for (let i = 0; i < path.length; i++) {
-        setTimeout(() => {
-          setMatrix((prev) => {
-            const newMatrix = [...prev]
-            if (i === path.length - 1) {
-              newMatrix[path[i].num].status = 1
-              return newMatrix
-            }
-            if (i === 0) {
-              newMatrix[path[i].num].status = 0
-              return newMatrix
-            }
-            newMatrix[path[i].num].status = 6
-            return newMatrix
-          })
-        }, 15 * i)
-      }
-    }, processSpeed * visitedNodes.length)
-  }
-
-
-
-  const startVisualization = () => {
-    if (alg === 0) {
-      const result = dijkstra(matrix, setMatrix);
-      Animate(result);
     }
-    if (alg === 1) {
-      // const result = aStar(matrix, setMatrix);
-      // Animate(result);
-      console.log("start A*")
-    }
-    
-  }
 
-  return  (
-    <div style={styles.main}>
-      <Bar alg={alg} setAlg={setAlg} disabledChoice={disabledChoice} setMode={setMode} mode={mode} start={startVisualization} clear={clearMatrix} />
-      <div style={styles.center}>
-        <div style={styles.grid}>
-          {matrix.map((node) => (
-            <Node disabledChoice={disabledChoice} setMatrix={setMatrix} mode={mode} key={node.num} num={node.num} status={node.status} />
-            ))}
+    return  (
+      <div style={styles.main}>
+        <Bar alg={alg} setAlg={setAlg} disabledChoice={disabledChoice} setMode={setMode} mode={mode} start={startVisualization} clear={clearMatrix} />
+        <div style={styles.center}>
+          <div style={styles.grid}>
+            {matrix.map((node) => (
+              <Node disabledChoice={disabledChoice} setMatrix={setMatrix} mode={mode} key={node.num} num={node.num} status={node.status} />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
  const styles = {
   main: {
